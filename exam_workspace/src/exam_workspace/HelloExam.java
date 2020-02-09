74452cdb3e232ec004924a8cfc78c6a312a352a6
우리집 <@DESKTOP-6NVOS9G>
2020-02-09 15:52:47 +0900

    github sample

---
+++ exam_workspace/src/exam_workspace/HelloExam.java
@@ -0,0 +1,12 @@
+package exam_workspace;
+
+public class HelloExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloExam!!");
+		System.out.println("HelloExam!!");
+		System.out.println("HelloExam!!");
+	}
+
+}
